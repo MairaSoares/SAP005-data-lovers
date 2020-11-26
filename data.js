6d8f73788477680b4224e9b124cec998a27b968f
Kaueny de Oliveira Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2020-11-26 09:25:50 -0300

    Filtros de ordenação

--- data.js
+++ data.js
@@ -1,9 +1,21 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return "example";
+window.app = {
+  sortData,
+  filterSearch,
 };
 
-export const anotherExample = () => {
-  return "OMG";
-};
+function sortData(data, sortBy, sortOrder) {
+  const copiedArray = data.slice();
+  if (sortOrder == "select-A-Z") {
+    return copiedArray.sort(function ordena(objOne, objTwo) {
+      return objOne[sortBy].localeCompare(objTwo[sortBy]);
+    });
+  } else if (sortOrder == "select-Z-A") {
+    return copiedArray.sort(function ordena(objOne, objTwo) {
+      return objTwo[sortBy].localeCompare(objOne[sortBy]);
+    });
+  }
+}
+
+function filterSearch(data, condition, key) {
+  return data.filter((item) => item[key].includes(condition));
+}
--- main.js
+++ main.js
@@ -1,52 +1,60 @@
-//import { pokemonBox } from "./data.js";
-import data from "./data/pokemon/pokemon.js";
-
 const divFilter = document.getElementById("order-type");
+const selectOrder = document.getElementById("order-name");
+const card = document.getElementById("list-cards");
 
-console.log(data);
-
-const severalCards = document.getElementById("cardsForm");
 const dataPoke = POKEMON.pokemon;
-let buscaPokemonData = pokemons; //Puxar do filtro todos os pokemons
 
-//função para os cards
-let pokemonBox = (name, number, type, image) => {
-  let typesFormated = formatTypes(type);
-  return `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-    <div class="card w-100 pokemon-box">
-    <div  class="card-img-top">
-    <img class="img-fluid" src="${image}" alt="${name}">
-    </div>
-    <div class="card-body">
-    <p class="card-text">#${number}</p>
-    <p class="card-text">${name}</p>
-    <p class="card-text">${typesFormated}</p>
-    </div>
-    </div>
-    </div>`;
-};
-//Organizar os tipos e para converter em array ou elementos independentes.
-let formatTypes = (types) => {
-  let typesHtml = "";
-  for (let i = 0; i < types.length; i++) {
-    //para mexer no CSS e para que apareçam lado a lado.
-    typesHtml += `<div class="tag-type bg-${types[i].toLowerCase()}">${
-      types[i]
-    }</div>`;
-  }
-  return typesHtml;
+//Carregar as imagens após a inicialização da uma página
+window.onload = () => {
+  listCard(dataPoke);
+  populateDropdown(dataPoke);
 };
 
-// passa a função pokemons para o HTML (colocar nome da let do filtro)
-let = (pokemons) => {
-  // Amonstra dos pokemons.
-  document.getElementById("list-cards").innerHTML = "";
-  for (let i = 0; i < pokemons.length; i++) {
-    document.getElementById("list-cards").innerHTML += pokemonBox(
-      pokemons[i].name,
-      pokemons[i].num,
-      pokemons[i].type,
-      pokemons[i].img
-    );
-  }
+//Função para renderizar o array de pokemons
+const listCard = (data) => {
+  card.innerHTML = data
+    .map(
+      (poke) => `
+    <section class="box">
+      <p>${poke.num}</p>
+      <h3 > ${poke.name}</h3>
+      <img src=${poke.img} />
+      <div>
+          <p><strong>Tipo:</strong> ${poke.type.join(", ")}</p>
+          <p><strong>Ovo:</strong> ${poke.egg}</p>
+          <p><strong>Chance:</strong> ${poke.avg_spawns} % </p>
+          <p><strong>Fraquezas:</strong> ${poke.weaknesses.join(", ")}</p>
+      </div>
+    </section>
+  `
+    )
+    .join("");
 };
+
+//Filtro dos pokemons de A-Z / Z-A
+selectOrder.addEventListener("change", ({ target: { value } }) => {
+  const functionOrder = window.sortData(dataPoke, "name", value);
+  if (value === "none") {
+    return listCard(dataPoke);
+  } else {
+    listCard(functionOrder);
+  }
+});
+
+//função para aparecer o typo dos pokemons nas opções de escolha
+const populateDropdown = (pokemons) => {
+  const pokemonsType = pokemons.map((poke) => poke.type).flat();
+  const uniqueTypes = [...new Set(pokemonsType)];
+  const optionsLayout = uniqueTypes
+    .map(
+      (type) => `
+    <option value=${type}>${type}</option>`
+    )
+    .join("");
+  document.querySelector("#order-type").innerHTML = optionsLayout;
+};
+
+//Filtro dos pokemons de acordo com o typo
+divFilter.addEventListener("change", (e) => {
+  listCard(filterSearch(dataPoke, e.target.value, "type"));
+});
